abdd077cfad3d50897c89716832e9d014de133d9
Jeong-ki <7863z@naver.com>
2022-04-04 23:21:02 +0900

    saga분리 후 reducer연결

--- front/components/Form/LoginForm.js
+++ front/components/Form/LoginForm.js
@@ -3,8 +3,8 @@ import { Form, Input, Button } from 'antd';
 import Link from 'next/link';
 import styled from 'styled-components';
 import useInput from '../../hooks/useInput';
-import { useDispatch } from 'react-redux';
-import { loginAction } from '../../reducers/user';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginRequestAction } from '../../reducers/user';
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
@@ -27,12 +27,13 @@ const FormWrapper = styled(Form)`
 
 const LoginForm = () => {
   const dispatch = useDispatch();
+  const { isLoggingIn } = useSelector((state) => state.user);
   const [id, onChangeId] = useInput('');
   const [password, onChangePassword] = useInput('');
 
   const onSubmitForm = useCallback((e) => {
     console.log(id, password);
-    dispatch(loginAction({ id, password }));
+    dispatch(loginRequestAction({ id, password }));
   }, [id, password]);
 
   return (
@@ -55,7 +56,7 @@ const LoginForm = () => {
           />
         </div>
         <ButtonWrapper>
-          <Button className="loginBtn" type="primary" htmlType="submit" loading={false}>로그인</Button>
+          <Button className="loginBtn" type="primary" htmlType="submit" loading={isLoggingIn}>로그인</Button>
           <Link href="/signup"><a><Button>회원가입</Button></a></Link>
         </ButtonWrapper>
       </fieldset>
