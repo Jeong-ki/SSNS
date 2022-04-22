f5e9dbe0c83bbe628bbdedb74a52ca48278a9b42
Jeong-ki <7863z@naver.com>
2022-04-22 23:28:55 +0900

    followm unfollow 버튼, 기능 구현

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -8,6 +8,7 @@ import PostImages from './PostImages';
 import CommentForm from './Form/CommentForm';
 import PostCardContent from './PostCardContent';
 import { REMOVE_POST_REQUEST } from '../reducers/post';
+import FollowButton from './FollowButton';
 
 const PostCard = ({ post }) => {
   const dispatch = useDispatch();
@@ -54,6 +55,7 @@ const PostCard = ({ post }) => {
             <EllipsisOutlined />
           </Popover>
         ]}
+        extra={id && <FollowButton post={post} />}
       >
         <Card.Meta
           avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
